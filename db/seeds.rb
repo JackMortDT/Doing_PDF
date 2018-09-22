7006862fd861931e6d852212fb7a094a7337f7df
LuisSas <luis@makingdevs.com>
2018-09-22 00:21:41 -0500

    Add 5 registers for fill the table of products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.create(title: 'Computer', pricing: 10000)
+Product.create(title: 'Mouse', pricing: 500)
+Product.create(title: 'Keyboard', pricing: 1000)
+Product.create(title: 'Usb', pricing: 300)
+Product.create(title: 'CD', pricing: 12)
